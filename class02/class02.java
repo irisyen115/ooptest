2da71788471c8832dcbc71426f3a79ed66115e7e
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-10-05 22:59:22 +0800

    revise mistake

--- class02/class02.java
+++ class02/class02.java
@@ -1,8 +1,8 @@
-package class02;
+package Class02;
 
 class Data {
-    private static String name;
-    private static Test score;
+    private String name;
+    private Test score;
 
     class Test {
         public int english;
@@ -14,45 +14,39 @@ class Data {
         }
 
         double avg() {
-            return (english + math) / 2;
+            return (double)(english + math) / 2;
         }
     }
 
     public Data(String n, int eng, int m) {
         name = n;
-        eng = score.english;
-        m = score.math;
+        score = new Test(eng, m);
     }
 
 
-    public static String getname() {
+    public String getname() {
         return name;
     }
 
-    public static Test getscore() {
+    public Test getscore() {
         return score;
     }    
-}
-
-class data extends Data {
-    public data(String n, int eng, int m) {
-        super(n, eng, m);
-    }
-
-    public static void show(Data[] d) {
+    public void show(Data[] d) {
         for (int i = 0; i < d.length; i++) {
             System.out.println(
-                    "name:" + getname() + " " + "english:" + getscore().english + " " + "math:" + getscore().math + " " + getscore().avg());
+                    "name:" + getname() + " " + "english:" + getscore().english + " " + "math:" + getscore().math + " " +"avg:"+ getscore().avg());
+                    break;
         }
     }
 }
 
-public class class02 {
+public class Class02 {
     public static void main(String[] args) {
         Data dat[];
         dat = new Data[2];
         dat[0] = new Data("Annie", 85, 92);
         dat[1] = new Data("Brian", 77, 56);
-        data.show(dat);
+        dat[0].show(dat);
+        dat[1].show(dat);
     }
 }
